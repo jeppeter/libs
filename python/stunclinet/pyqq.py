a3dec1b019bc87ca677f7cc036143369bbc72179
jeppeter <jeppeter@gmail.com>
2013-01-20 05:31:33 +0800

    to add the http request

--- python/stunclinet/pyqq.py
+++ python/stunclinet/pyqq.py
@@ -10,7 +10,28 @@ class PyQQ:
 	def Request(self,method,url,data={})
 		try:
 			_url = httplib.urlsplit(url)
-			_conn = 
+			_server,_port = _url.netloc.split(':')
+			if _port is None:
+				_port = 80
+			_conn = httplib.HTTPConnection(_server,_port,True,3)
+			_conn.connect()
+			data = urllib.urlencode(data)
+			if method == 'get':
+				_conn.putrequest("GET",url,None)
+				_conn.putheader("Content-Length",'0')
+			elif method == 'post':
+				_conn.putrequest("POST",url)
+				_conn.putheader("Content-Length", str(len(data)))
+				_conn.putheader("Content-Type", "application/x-www-form-urlencoded")
+			_conn.putheader("Connection", "close")
+			_conn.endheaders()
+
+			if len(data) > 0:
+				_conn.send(data)
+			f = _conn.getresponse()
+			self.httpBody = f.read().encode('gbk')
+			f.close()
+			_conn.close()
 		except:
 			self.httpBody = ''
 		return self.httpBody
