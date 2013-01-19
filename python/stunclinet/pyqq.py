ebfbf4d548e663c6c40113420897f2df14d3444a
jeppeter <jeppeter@gmail.com>
2013-01-19 20:24:06 +0800

    init pyqq to make the python script for qq 3g reencapsulation

---
+++ python/stunclinet/pyqq.py
@@ -0,0 +1,17 @@
+#! python
+#coding:utf-8
+
+import httplib
+
+class PyQQ:
+	def __init__(self):
+		pass
+
+	def Request(self,method,url,data={})
+		try:
+			_url = httplib.urlsplit(url)
+			_conn = 
+		except:
+			self.httpBody = ''
+		return self.httpBody
+
