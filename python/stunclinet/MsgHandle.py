b78bfe81a5b31ee4f1515625af0150841b9b42cb
jeppeter <jeppeter@gmail.com>
2013-01-26 17:38:19 +0800

    to add function talbe in the qqcon.py

--- python/stunclinet/MsgHandle.py
+++ python/stunclinet/MsgHandle.py
@@ -7,37 +7,57 @@
 class MessageHandleException(Exception):
 	pass
 
-__CmdHelpDict = {
-	'version' : 'version 1.0',
-	'connectstun' : 'connect [stunserver] local_bind_port' ,
-	'sendudp' : 'send local_bind_port remote_ip remote_port [send_message] default message is "openvpn verify message"',
-	'startcmd' : 'start args [...] run the command',
-	'localudpproxy' : 'proxy local_listen_port [local_ip] local_forward_port ; in the middle of the proxy of udp',
-	'help' : 'to display total cmd help message',
-}
 
 class MessageHandle:
 	def __init__(self):
 		self._resp = ''
 		self._HasProcessed = 0
+		self.__CmdHelpDict = {
+		'version' : 'version 1.0',
+		'connectstun' : 'connect [stunserver] local_bind_port' ,
+		'sendudp' : 'send local_bind_port remote_ip remote_port [send_message] default message is "openvpn verify message"',
+		'startcmd' : 'start args [...] run the command',
+		'localudpproxy' : 'proxy local_listen_port [local_ip] local_forward_port ; in the middle of the proxy of udp',
+		'help' : 'to display total cmd help message',
+		}
+		self.__CmdFuncTable ={
+			'version'         : MessageHandle.__Version,
+			'connectstun'     : MessageHandle.__ConnectStun,
+			'sendudp'         : MessageHandle.__SendUdp,
+			'startcmd'        : MessageHandle.__StartCmd,
+			'localudpproxy'   : MessageHandle.__LocalUdpProxy,
+			'help'            : MessageHandle.__Help
+		}
+
+	def __Version(self,args):
+		pass
+	def __ConnectStun(self,args):
+		pass
+	def __SendUdp(self,args):
+		pass
+	def __StartCmd(self,args):
+		pass
+	def __LocalUdpProxy(self,args):
+		pass
+	def __Help(self,args):
+		if args is None:
+			for a in self.__CmdHelpDict.keys():
+				if a != 'help':
+					self._resp += self.__CmdHelpDict[a]
+					self._resp += '\n'
+		else:
+			for a in args:
+				if a in self.__CmdHelpDict.keys():
+					self._resp += self.__CmdHelpDict[a]
+					self._resp += '\n'
+				else:
+					raise MessageHandleException('unkown %s please use help for more information'%(repr(a)))
+		return self._resp
+
 
 	def GetResponse(self):
 		return self._resp
 
-	def __HelpCmd(self,cmdname):
-		if cmdname in __CmdHelpDict.keys():
-			self._resp += __CmdHelpDict[cmdname]
-		else:
-			raise MessageHandleException('unknown command name %s'%(cmdname))
-		return self._resp
-	
-	def __Help(self,cmds):
-		if 'help' in cmds:
-			for c in __CmdHelpDict.keys():
-				if c != 'help':
-					self.__HelpCmd(c)
-		else:
-			return self.__HelpCmd(cmds[1])
 			
 
 	def ProcessHandler(self,cmds=None):
@@ -49,11 +69,8 @@ class MessageHandle:
 			raise MessageHandleException('cmds less than 1')
 
 		# now to make sure that the handle
-		if cmds[0] == 'version':
-		elif cmds[0] == 'help':
-		elif cmds[0] == 'connectstun':
-		elif cmds[0] == 'listenport':
-		elif cmds[0] == 'sendudp':
+		if cmds[0] in self.__CmdFuncTable.keys():
+			self.__CmdFuncTable[cmds[0]](self,cmds[1:])
 		else:
 			raise MessageHandleException('cmds[0] %s not recognize ,please use help to know'%(cmds[0]))
 
