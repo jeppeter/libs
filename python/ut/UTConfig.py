109f183e47018b36fd147742ff69ea14acd32d23
jeppeter <jeppeter@gmail.com>
2013-04-11 17:19:32 +0800

    to add section add in UTConfig

--- python/ut/UTConfig.py
+++ python/ut/UTConfig.py
@@ -2,6 +2,7 @@
 
 import ConfigParser
 import LocalException
+import logging
 
 class UTCfgKeyError(LocalException.LocalException):
 	pass
@@ -24,6 +25,21 @@ class UTConfig:
 		self.__IncludeFiles = []
 		assert( self.__FuncLevel == 0)
 
+
+	def __AddOption(m,p):
+		for s in p.sections():
+			for c in p.options(s):
+				if m.has_section(s):
+					if m.has_option(s,c):
+						logging.warning('redefined [%s].%s'%(s,c))
+						m.set(s,c,p.get(s,c))
+					else:
+						m.set(s,c,p.get(s,c))
+				else:
+					m.add_section(s)
+					m.set(s,c,p.get(s,c))
+		return m
+
 	def __LoadFile(self,fname):
 		'''
 			this is to load the files to the config
@@ -32,6 +48,7 @@ class UTConfig:
 		'''
 		if self.__FuncLevel >= 30:
 			raise UTCfgOverflowError('Load %s fname overflow'%(fname))
+		# we parse the file
 		
 
 	def __GetValue(self,section,item,expand=1,level=0):
