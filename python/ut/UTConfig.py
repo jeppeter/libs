896b6f11f4495f24180cbfb7bcfd12ae6e2cfd4b
jeppeter <jeppeter@gmail.com>
2013-04-12 15:16:48 +0800

    add get value in UTConfig.py

--- python/ut/UTConfig.py
+++ python/ut/UTConfig.py
@@ -3,6 +3,8 @@
 import ConfigParser
 import LocalException
 import logging
+import sys
+import re
 
 class UTCfgKeyError(LocalException.LocalException):
 	pass
@@ -13,10 +15,13 @@ class UTCfgOverflowError(LocalException.LocalException):
 class UTConfig:
 	def __init__(self,fname=None):
 		self.__IncludeFiles = []
+		self.__SearchPaths=[]
+		self.__UnitTests = []
 		self.__FuncLevel = 0
 		self.__MainCfg = None
 		if fname :
 			self.__LoadFile(fname)
+			assert(self.__FuncLevel == 0)
 			self.__MainName = fname
 		else:
 			self.__MainName = None
@@ -41,8 +46,41 @@ class UTConfig:
 					m.set(s,c,p.get(s,c))
 		return m
 
-	def __ParseIncludeFiles(self,cfg):
-		pass
+
+	def __AddSearchPathSection(self,cfg):
+		s = 'search_path'
+		if cfg.has_section(s):
+			# now to add the search path
+			for c in cfg.options(s):
+				v = cfg.get(s,c)
+				if c not in self.__SearchPaths:
+					self.__SearchPaths.append(c)
+					sys.path.append(c)
+		return
+
+	def __AddUnitTestSection(self,cfg):
+		s = 'unit_test'
+		if cfg.has_section(s):
+			for c in cfg.options(s):
+				v = cfg.get(s,c)
+				if v == 'y' and v not in self.__UnitTests:
+					self.__UnitTests.append(c)
+		return
+					
+
+	def __AddIncludeFiles(self,cfg):
+		# now we should plus the func level for including
+		s = 'include_files'
+		if cfg.has_section(s):
+			for c in cfg.options(s):
+				v = cfg.get(s,c)
+				if v == 'y' :
+					try:
+						self.__FuncLevel += 1
+						sefl.__LoadFile(c)
+					finally:
+						self.__FuncLevel -= 1
+		return
 
 	def __LoadFile(self,fname):
 		'''
@@ -68,19 +106,35 @@ class UTConfig:
 
 		############################
 		#load special config 
-		# include_files
-		# unit_test_modules
+		# [search_path]
+		# [include_files]
+		# [unit_test]
+		# sections to be the value
 		############################
+		self.__AddSearchPathSection(cfg)
+		self.__AddIncludeFiles(cfg)
+		self.__AddUnitTestSection(cfg)
+		
 		self.__AddOption(self.__MainCfg,cfg)
 		return 
 			
 
-	def __GetValue(self,section,item,expand=1,level=0):
+	def __GetValue(self,section,item,expand=1,valuemap={}):
 		'''
 			the  section name ,and expand will expand the value of 
 			expand == 1 : will expand the value '%(value)s' to the real value
-			level will be 
+			level will be expand to it
 		'''
-		pass
-
-	
+		if self.__MainCfg is None:
+			return None
+		v = None
+		if self.__MainCfg.has_section(section):
+			if self.__MainCfg.has_option(section,item):
+				# now we should give the value expand
+				if expand :
+					# now expand ,so we should expand value
+					tmpv = self.__MainCfg.get(section,item,0)
+				else:
+					# now expand ,so we get the raw value
+					v = self.__MainCfg.get(section,item,0)
+		return v
