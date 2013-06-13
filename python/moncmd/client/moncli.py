e7d3675db7e16cbff76de48dc1c3fdad33d36ab7
jeppeter <jeppeter@gmail.com>
2013-06-14 06:40:58 +0800

    to make the option parser in the repo

--- python/moncmd/client/moncli.py
+++ python/moncmd/client/moncli.py
@@ -2,9 +2,22 @@
 
 import sys
 from optparse import OptionParser
+import os
+def Usage(ec,opt,msg=None):
+	fp = sys.stderr
+	if ec == 0 :
+		fp = sys.stdout
+	if msg is not None:
+		fp.write('%s\n'%(msg))
+	fp.write('%s [OPTIONS]\n'%(os.path.basename(__file__)))
+	fp.write('\t-H host    : specify the hostname and port host:port\n')
+	fp.write('\t-p  process : specify the process to handle\n')
+	sys.exit(ec)
+	return
 
 def main():
-	pass
+	args = OptionParser()
+	args.add_option
 
 if __name__ == '__main__':
 	main()
