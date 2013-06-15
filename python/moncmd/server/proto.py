04f648687487a5d8377fd9d588823b8a319fe448
jeppeter <jeppeter@gmail.com>
2013-06-15 21:51:05 +0800

    add proto handle in proto.py

--- python/moncmd/server/proto.py
+++ python/moncmd/server/proto.py
@@ -7,6 +7,9 @@
 '''
 
 import socket
+import LocalException
+class MonRecvError(LocalException.LocalException):
+	pass
 class MonProtoSock:
 	def __init__(self,sock):
 		self.__sock = sock
@@ -15,5 +18,14 @@ class MonProtoSock:
 	def GetSocket(self):
 		return self.__sock
 	def ReadMessage(self):
+		assert(self.__sock)
+		buf = self.__sock.recv(8192)
+		if len(buf) < 2:
+			raise MonRecvError('receive error')
+		size = struct.unpack('<H',buf[:2])
+		while len(buf) < (size + 2):
+			buf += self.__sock.recv(8192)
+		type,msg = struct.unpack('<HS*',buf[2:4],buf[4:])
+		return type,msg
 
 	def WriteMessage(self,type,msg):
