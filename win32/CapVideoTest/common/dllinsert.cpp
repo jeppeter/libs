c57cf1d2fdf0bb24bbd6f57bbd4ecffeb12205f3
jeppeter <jeppeter@gmail.com>
2013-08-11 16:37:14 +0800

    to add the module to handle verify

--- win32/CapVideoTest/common/dllinsert.cpp
+++ win32/CapVideoTest/common/dllinsert.cpp
@@ -60,10 +60,70 @@ extern "C" int LoadInsert(const char* pExec,const char* pDllFullName,const char*
 }
 
 
-extern "C" int CaptureFile(DWORD processid,const char* bmpfile)
+int __IsModuleIn(DWORD processid,const char* pDllName,const char* pDllFullName)
+{
+    HANDLE hsnap=INVALID_HANDLE_VALUE;
+    PMODULEENTRY32 *pModule32=NULL;
+    HANDLE hProcess=NULL;
+    int ret=0;
+    BOOL bret;
+    TCHAR *pFullName=NULL;
+#ifdef _UNICODE
+    char* pFullAnsi=NULL;
+    pFullAnsi = new char[MAX_MODULE_NAME32 + 1+MAX_PATH];
+#endif
+    pModule32 = new MODULEENTRY32;
+    pFullName = new TCHAR[MAX_MODULE_NAME32 + 1+MAX_PATH];
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS,FALSE,processid);
+    if (hProcess == NULL)
+    {
+        ret = 0;
+        goto out;
+    }
+
+    hsnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE,processid);
+    if (hsnap == INVALID_HANDLE_VALUE)
+    {
+        ret = 0;
+        goto out;
+    }
+
+    for (bret = Module32First(hsnap,pModule32); bret ; bret = Module32Next(hsnap,pModule32))
+    {
+#ifdef _UNICODE
+	/*now we should copy the full name*/
+	bret = 
+#else
+#endif
+    }
+
+
+
+out:
+    if (hsnap != INVALID_HANDLE_VALUE)
+    {
+        CloseHandle(hsnap);
+    }
+    hsnap = INVALID_HANDLE_VALUE;
+    if (hProcess != NULL)
+    {
+        CloseHandle(hProcess);
+    }
+    hProcess = NULL;
+    delete pModule32;
+    delete [] pFullName ;
+#ifdef _UNICODE
+    delete [] pFullAnsi;
+#endif
+    return 0;
+}
+
+extern "C" int CaptureFile(DWORD processid,const char* bmpfile,const char* pDllName,const char* pDllFullName)
 {
     HANDLE hProcess= NULL;
     int ret;
+    PVOID pParam=NULL;
+    DWORD paramsize=0;
 
     hProcess = OpenProcess(PROCESS_ALL_ACCESS,FALSE,processid);
     if (hProcess == NULL)
@@ -75,6 +135,13 @@ extern "C" int CaptureFile(DWORD processid,const char* bmpfile)
 
     return 0;
 fail:
+    if (pParam)
+    {
+        assert(hProcess != NULL);
+        VirtualFreeEx(hProcess,pParam,paramsize,MEM_RELEASE);
+    }
+    pParam = NULL;
+    paramsize = 0;
     if (hProcess != NULL)
     {
         CloseHandle(hProcess);
