4b4b3c32e328a9f6a9fa056a78d948ba6b5447ea
jeppeter <jeppeter@gmail.com>
2013-08-13 20:54:14 +0800

    to add the main function in finddll

--- win32/finddll/finddll/finddll.cpp
+++ win32/finddll/finddll/finddll.cpp
@@ -4,8 +4,54 @@
 #include "stdafx.h"
 
 
+static int st_AnsiArgc;
+static char *st_AnsiArgv[]=NULL;
+
+char*** AnsiArgs(int argc,wchar_t* argv[])
+{
+	int retargv=argc;
+	char** ppArgv=NULL;
+	int i;
+
+	ppArgv = calloc(sizeof(*ppArgv),(argc+1));
+	if (ppArgv == NULL)
+	{
+		goto fail;
+	}
+
+	for (i=0;i<argc;i++)
+	{
+	}
+
+	return ppArgv;
+fail:
+	if (ppArgv)
+	{
+		for(i=0;i<argc;i++)
+		{
+			if (ppArgv[i])
+			{
+				free(ppArgv[i]);
+			}
+			ppArgv[i]=NULL;
+		}
+		free(ppArgv);
+	}
+	ppArgv =NULL;
+	
+	return NULL;
+}
+
+
+
+int AnsiMain(int argc,char *argv[])
+{
+
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	
 	return 0;
 }
 
