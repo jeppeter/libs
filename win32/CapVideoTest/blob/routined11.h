b85f3fc1f8139a532c5bc210a72e9986d630a00d
jeppeter <jeppeter@gmail.com>
2013-08-21 09:14:28 +0800

    add routined11.h routined11.cpp into repo

---
+++ win32/CapVideoTest/blob/routined11.h
@@ -0,0 +1,11 @@
+
+#ifndef __ROUTINE_D11_H__
+#define __ROUTINE_D11_H__
+
+int RoutineDetourD11(void);
+
+EXTERN_C __declspec(dllexport) int Capture2DBackBufferD11(const char* filetosave);
+
+
+#endif /*__ROUTINE_D11_H__*/
+
