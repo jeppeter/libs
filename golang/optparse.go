04280b13729b960652800e31e919a3df4a0997f5
jeppeter <jeppeter@gmail.com>
2014-01-09 19:02:42 +0800

    to add option parser struct

--- golang/optparse.go
+++ golang/optparse.go
@@ -1,5 +1,22 @@
 package optparse
 
 type OptionParser struct{
+
+}
+
+type Option_add_param struct{
+	flags []string
+	help string
+	defvalue string
+	callback bool
+	store bool
+	store_const bool
+	store_true bool
+	append bool
+	append_const bool
+	count bool
+
+}
+func  (o *OptionParser) Add_option(opt option_add_param) e error{
 	
 }
